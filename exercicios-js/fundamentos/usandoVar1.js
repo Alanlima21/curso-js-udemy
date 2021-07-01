5cafd47ffeb46b908cbf352cd955c7ba603ee13f
Alan lima <alan.lima@capgemini.com>
2021-07-01 14:23:56 -0300

    usando var 1

---
+++ exercicios-js/fundamentos/usandoVar1.js
@@ -0,0 +1,19 @@
+{
+    {
+        {
+            {
+                var sera = 'Será??'; // escopo global
+            }
+        }
+    }
+}
+console.log(sera);  
+
+
+function teste(){
+    var local = 123; // escopo de função
+    console.log(local);
+}
+
+teste();
+// erro - console.log(local);
