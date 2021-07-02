57b55fbc9ff0b1d2dbdc4df9aa093396612de9b5
Alan lima <alan.lima@capgemini.com>
2021-07-02 13:26:12 -0300

    Relacionais

--- exercicios-js/fundamentos/destructuring4.js
+++ exercicios-js/fundamentos/destructuring4.js
@@ -1,5 +1,5 @@
 function rand([min = 0, max = 1000]){  //destructuring
-    if(min > max) [min, max] = [max, min];  // troca o valor caso o min seja maior
+    if(min > max) [min, max] = [max, min];  // troca o valor caso o min seja maior usando destructuring
     const valor = Math.random() * (max - min) + min;
     return Math.floor(valor);
 }
