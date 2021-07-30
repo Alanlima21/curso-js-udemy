18c37dc648f398aa691f15700390a0350e53dc2e
Alan lima <alan.lima@capgemini.com>
2021-07-30 11:41:55 -0300

    exercicio 10

---
+++ exercicios-js/exercicios/10.js
@@ -0,0 +1,12 @@
+const impar = (numero) =>{
+   if(numero % 3 == 0){
+       return true;
+   }else{
+       return false;
+   }
+}
+
+
+console.log(impar(38));
+console.log(impar(27));
+console.log(impar(9));
