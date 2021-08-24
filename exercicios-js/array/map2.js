3694b39085ec60cc011cca30058c5dd8822fb40e
Alan lima <alan.lima@capgemini.com>
2021-08-24 16:04:58 -0300

    filter 1

--- exercicios-js/array/map2.js
+++ exercicios-js/array/map2.js
@@ -5,6 +5,5 @@ const carrinho =[
     '{"nome": "Caneta", "preco": 7.50}'
 ];
 
-let obj = carrinho.map(obj => JSON.parse(obj));
-let precos = obj.map(obj => obj.preco);
+let precos = carrinho.map(obj => JSON.parse(obj)).map(obj => obj.preco);
 console.log(precos);
