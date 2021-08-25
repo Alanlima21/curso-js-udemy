5363ac396c82da8753d3fe45f6339ed0344af747
Alan lima <alan.lima@capgemini.com>
2021-08-25 14:02:49 -0300

    exercicios 2

---
+++ exercicios-js/exercicios2/03.js
@@ -0,0 +1,5 @@
+function calcularSalario(qtdHoras, valorHora){
+    console.log(`Salário igua a R$ ${qtdHoras * valorHora}`);
+}
+
+calcularSalario(150, 40.5);
---
+++ exercicios-js/exercicios2/05.js
@@ -0,0 +1,7 @@
+function maiorOuIgual(num1, num2){
+    console.log(num1 >= num2 && typeof num1 === typeof num2);
+}
+
+maiorOuIgual(0,0);
+maiorOuIgual(0,'0');
+maiorOuIgual(5,1);
