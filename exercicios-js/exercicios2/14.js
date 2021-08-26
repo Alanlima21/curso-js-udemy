3d322f5c39d5c76787080f875cbef1b073deadad
Alan lima <alan.lima@capgemini.com>
2021-08-26 15:47:40 -0300

    exercicio 14

---
+++ exercicios-js/exercicios2/14.js
@@ -0,0 +1,23 @@
+function objetoParaArray(obj){
+   let array = [];
+  
+   
+    Object.keys(obj).forEach(function(item){
+        array.push([].concat(item + " = " + obj[item]));
+    });
+
+   /* for(let prop in obj){
+        array.push([].concat( prop + " = " + obj[prop]));
+    }*/
+    console.log(array);
+}
+
+objetoParaArray({
+    nome: "Maria",
+    profissao: "Desenvolvedora de software"
+    })
+
+    objetoParaArray({
+        codigo: 11111,
+        preco: 12000
+        })
