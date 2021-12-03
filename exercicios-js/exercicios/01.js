33b510abb8aae054768d73eb56326d4195224824
Alan lima <alan.lima@capgemini.com>
2021-12-03 11:10:35 -0300

    For in e For of

--- exercicios-js/exercicios/01.js
+++ exercicios-js/exercicios/01.js
@@ -6,3 +6,16 @@
     }
 
     operacoes(25,2);
+
+    const array=['ABC','DEF']
+ 
+for(let i in array){
+    console.log(i)
+}
+//esse for vai retornar os índices do array que no caso são 0,1
+ 
+const array2=['A','B','C']
+ 
+for(let e of array2){
+    console.log(e)
+}
